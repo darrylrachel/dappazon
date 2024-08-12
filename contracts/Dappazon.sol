2db84b478a77041fba795dc38e9bd538fee14c83
Darryl Rachel <racheldarryl@icloud.com>
2024-08-12 11:43:46 -0700

    done: setup and initial tests

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.9; // Remove for security purposes before final commit
 
-contract Dappazon {}
+contract Dappazon {
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+                    Are written to the blockchain itself.              
+    //////////////////////////////////////////////////////////////*/
+    string public name; // Used to test setup, can delete afterwards
+    address public owner;
+
+    // The constructor function is called only once when the smart contract is deployed to the blockchain and is charged a gas fee
+    constructor() {
+        name = "Dappazon"; // Used to test setup, can delete afterwards
+
+        // Owner of smart contract who receives the funds
+        owner = msg.sender; // msg.sender is the person deploying the smart contract
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             LIST PRODUCTS
+  //////////////////////////////////////////////////////////////*/
+    function list(
+        uint256 _id, // Unique identifier [number]
+        string memory _name, // Name of product
+        string memory _category, // Product category
+        string memory _image, // Product image
+        uint256 _cost, // Product cost
+        uint256 _rating, // Customer ratings
+        uint256 _stock // # of units in stock
+    ) public {}
+
+    /*//////////////////////////////////////////////////////////////
+                             BUY PRODUCTS
+  //////////////////////////////////////////////////////////////*/
+
+    /*//////////////////////////////////////////////////////////////
+                             WITHDRAW FUNDS
+  //////////////////////////////////////////////////////////////*/
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,38 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
 
+  beforeEach(async () => {
+    // Setup and fetch accounts with ethers.js, npx hardhart node shows 20 test accounts from ethers
+    [deployer, buyer] = await ethers.getSigners()
+    // console.log(deployer.address, buyer.address)
+
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+
+
+  })
+
+
+  describe("Deployment", () => {
+
+    it('sets the owner', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+
+    // Used to test setup, can delete afterwards
+    it('has a name', async () => {
+      const name = await dappazon.name()
+      expect(name).to.equal("Dappazon")
+    })
+  })
+  
 })
