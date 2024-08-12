3a20db7c9218831e27b8616be1873bd6b7196807
Darryl Rachel <racheldarryl@icloud.com>
2024-08-12 12:54:00 -0700

    done: basic listing function, test, refactored tests, emit List event, mapped Item struct

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -6,12 +6,54 @@ contract Dappazon {
                             STATE VARIABLES
                     Are written to the blockchain itself.              
     //////////////////////////////////////////////////////////////*/
-    string public name; // Used to test setup, can delete afterwards
+
+    // Used to test setup, can delete afterwards
+    // string public name;
+
     address public owner;
 
-    // The constructor function is called only once when the smart contract is deployed to the blockchain and is charged a gas fee
+    // Structs lets you create your own custom data types
+    // This struct "defines" what the struct will look like, it does not create one.
+    // The one below creates the struct
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                MAPPINGS
+
+    Let's us treat the blockchain like a database
+    mappings are key-value pairs
+    This mapping create a new mapping with the key data type uint256 and the Item struc as it's value
+    This allows for saving this Item to the blockchain using this mapping with a unique id(key)
+    It is saved to a public state variable called items so it can be accessed such as displaying the product on a website
+    //////////////////////////////////////////////////////////////*/
+    mapping(uint256 => Item) public items;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+
+    Benefits of events
+      1. Anytime function is called someone can subscribe and get notified ex. push notification
+      2. You can see every time the function was called on the blockchain by fetching the event stream
+    //////////////////////////////////////////////////////////////*/
+    event List(string name, uint256 cost, uint256 quantity);
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+
+    The constructor function is called only once when the smart contract is deployed to the blockchain 
+    and is charged a gas fee
+    //////////////////////////////////////////////////////////////*/
     constructor() {
-        name = "Dappazon"; // Used to test setup, can delete afterwards
+        // Used to test setup, can delete afterwards
+        // name = "Dappazon";
 
         // Owner of smart contract who receives the funds
         owner = msg.sender; // msg.sender is the person deploying the smart contract
@@ -28,7 +70,32 @@ contract Dappazon {
         uint256 _cost, // Product cost
         uint256 _rating, // Customer ratings
         uint256 _stock // # of units in stock
-    ) public {}
+    ) public {
+        // Restrict listing to only the owner of the contract / marketplace
+        // Has to evaluate to true or false
+        require();
+
+        // Create Item struct, "item" creates a new variable called item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save Item struct to blockchain using mapping
+        // items is accessed from the mapping above and is grapping the _id, it is then saving it to the item variable from creating the Item struct
+        items[_id] = item;
+
+        // Emitting an event
+        // Benefits
+        // 1. Anytime function is called someone can subscribe and get notified ex. push notification
+        // 2. You can see every time the function was called on the blockchain by fetching the event stream
+        emit List(_name, _cost, _stock);
+    }
 
     /*//////////////////////////////////////////////////////////////
                              BUY PRODUCTS
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,22 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+// Converts ETH number to smaller WEI number
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
+
+/*//////////////////////////////////////////////////////////////
+                  GLOBAL CONSTANTS FOR LISTING AN ITEM
+//////////////////////////////////////////////////////////////*/
+const ID = 1
+const NAME = 'Shoes'
+const CATEGORY = 'Clothing'
+const IMAGE = 'https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg'
+const COST = tokens(1) // 1 ETh, will be converted to WEI using tokens function above
+const RATING = 4
+const STOCK = 5
+    
 
 describe("Dappazon", () => {
   let dappazon
@@ -17,10 +30,11 @@ describe("Dappazon", () => {
     // Deploy contract
     const Dappazon = await ethers.getContractFactory("Dappazon")
     dappazon = await Dappazon.deploy()
-
-
   })
 
+  /*//////////////////////////////////////////////////////////////
+                            DEPLOYMENT TEST
+  //////////////////////////////////////////////////////////////*/
 
   describe("Deployment", () => {
 
@@ -29,9 +43,53 @@ describe("Dappazon", () => {
     })
 
     // Used to test setup, can delete afterwards
-    it('has a name', async () => {
-      const name = await dappazon.name()
-      expect(name).to.equal("Dappazon")
+    // it('has a name', async () => {
+    //   const name = await dappazon.name()
+    //   expect(name).to.equal("Dappazon")
+    // })
+  })
+
+  /*//////////////////////////////////////////////////////////////
+                            LISTING TEST
+  //////////////////////////////////////////////////////////////*/
+
+  describe("Listing", async () => {
+    let transaction
+    
+    beforeEach(async () => {
+     transaction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+
+      await transaction.wait()
+    })
+
+    // it('returns item attributes', async () => {
+    //   const item = await dappazon.items(1)
+    //   expect(item.id).to.equal(1)
+    //   expect(item.name).to.equal("Shoes")
+    // })
+
+    // Refactored
+    it('returns item attributes', async () => {
+      const item = await dappazon.items(ID)
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+
+    it('emits List events', () => {
+      expect(transaction).to.emit(dappazon, 'List')
     })
   })
   
